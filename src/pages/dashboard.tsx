112f8d76cb707240ceaa05dedd7cf586cef9542c
thanosan23 <p.thanosan23@gmail.com>
2024-04-28 11:36:23 -0400

    fix: added head to dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,17 @@
 
+import Head from "next/head";
+
 const Dashboard = () => {
     return (
         <>
-           <p>Dashboard here.</p> 
+        <Head>
+            <title>Dashboard | Flourish</title>
+            <meta name="description" content="Flourish" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+            <main className="p-4">
+                <p>Dashboard here.</p> 
+            </main>
         </>
     )
 }
